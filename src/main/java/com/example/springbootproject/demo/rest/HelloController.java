8868cf7fb477f63e4ca0190e1467375f3bae892e
Tamas <tamas.palszekely@attentioncrm.com>
2026-06-08 09:56:22 +0300

    myMessage

--- src/main/java/com/example/springbootproject/demo/rest/HelloController.java
+++ src/main/java/com/example/springbootproject/demo/rest/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/whattodo")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/today")
     public String helloController() {
         return "Fuck it! Today we'll see Max Amini!!!";
     }
